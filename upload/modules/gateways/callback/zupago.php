5e4143342ad849d943ab83d2f0ffcd4e30b76f26
HyBrid (HD) Wallet <Zpeter@Peters-MacBook-Pro.local>
2017-04-01 18:56:39 +0100

    Whmcs New Update

--- upload/modules/gateways/callback/zupago.php
+++ upload/modules/gateways/callback/zupago.php
@@ -3,64 +3,74 @@ include("../../../init.php");
 include("../../../includes/functions.php");
 include("../../../includes/gatewayfunctions.php");
 include("../../../includes/invoicefunctions.php");
-
 $gatewaymodule = "zupago";
 $GATEWAY = getGatewayVariables($gatewaymodule);
-if (!$GATEWAY["type"]) die("Module Not Activated"); 
+if (!$GATEWAY["type"])
+    die("Module Not Activated");
 
-function processPOST($apikey, $amount, $zupayee, $zupayee_btc, $cur){
+logTransaction($GATEWAY["name"], $_REQUEST, "Log"); # Save to Gateway Log: name, data array, status
 
-		$_POST['whmcs_apikey_compare']='CAME: '.$_POST['ZUPAYEE_ACC_KEY'].'; WE HAVE: '.$apikey;
-		$_POST['whmcs_amount_compare']='CAME: '.$_POST['PAYMENT_AMOUNT'].'; WE HAVE: '.$amount;
-		$_POST['whmcs_zupayee_compare']='CAME: '.$_POST['ZUPAYEE_ACC'].'; WE HAVE: '.$uzpayee;
-		$_POST['whmcs_zupayee_btc_compare']='CAME: '.$_POST['ZUPAYEE_ACC_BTC'].'; WE HAVE: '.$zupayee_btc;
-		$_POST['whmcs_currency_compare']='CAME: '.$_POST['CURRENCY_TYPE'].'; WE HAVE: '.$cur;
+function processPOST($apikey, $amount, $zupayee, $zupayee_btc, $cur) {
 
+    $_POST['whmcs_apikey_compare'] = 'CAME: ' . $_POST['ZUPAYEE_ACC_KEY'] . '; WE HAVE: ' . $apikey;
+    $_POST['whmcs_amount_compare'] = 'CAME: ' . $_POST['PAYMENT_AMOUNT'] . '; WE HAVE: ' . $amount;
+    $_POST['whmcs_zupayee_compare'] = 'CAME: ' . $_POST['ZUPAYEE_ACC'] . '; WE HAVE: ' . $uzpayee;
+    $_POST['whmcs_zupayee_btc_compare'] = 'CAME: ' . $_POST['ZUPAYEE_ACC_BTC'] . '; WE HAVE: ' . $zupayee_btc;
+    $_POST['whmcs_currency_compare'] = 'CAME: ' . $_POST['CURRENCY_TYPE'] . '; WE HAVE: ' . $cur;
 }
-$apikey=$GATEWAY["ZUPAYEE_ACC_KEY"];
 
-if($apikey=$_POST['ZUPAYEE_ACC_KEY']){ // proccessing payment if only apikey is valid
+$apikey = $GATEWAY["zupago_pass"];
+if ($_REQUEST['page'] == 'cancel') {
+    $paymentSuccess = false;
+    $invoiceid = (int) $_POST["PAYMENT_REF"];
+} else {
+    if ($apikey == $_POST['ZUPAYEE_ACC_KEY']) { // proccessing payment if only apikey is valid
+        $invoiceid = (int) $_POST["PAYMENT_REF"];
+        $invoiceid = checkCbInvoiceID($invoiceid, $GATEWAY["name"]); # Checks invoice ID is a valid invoice number or ends processing
+        checkCbTransID($_POST['tokan']); # Checks transaction number isn't already in the database and ends processing if it does
 
-	$invoiceid = (int)$_POST["PAYMENT_REF"];
-	$invoiceid = checkCbInvoiceID($invoiceid,$GATEWAY["name"]); # Checks invoice ID is a valid invoice number or ends processing
-	checkCbTransID($_POST['tokan']); # Checks transaction number isn't already in the database and ends processing if it does
 
-	
-	$qry=mysql_query("SELECT tblinvoices.total AS total, tblcurrencies.code AS currency_code, tblcurrencies.id AS currency_id FROM tblinvoices, tblclients, tblcurrencies WHERE tblinvoices.paymentmethod='$gatewaymodule' AND tblinvoices.id=$invoiceid AND tblinvoices.userid=tblclients.id AND tblclients.currency=tblcurrencies.id");
+        $qry = mysql_query("SELECT tblinvoices.total AS total, tblcurrencies.code AS currency_code, tblcurrencies.id AS currency_id FROM tblinvoices, tblclients, tblcurrencies WHERE tblinvoices.paymentmethod='$gatewaymodule' AND tblinvoices.id=$invoiceid AND tblinvoices.userid=tblclients.id AND tblclients.currency=tblcurrencies.id");
 
-	if(!$qry){ logTransaction($GATEWAY["name"], array_merge($_POST, array('SQL query'=>$qry)), "SQL query error"); die(); }
-	if(mysql_num_rows($qry)!=1){ logTransaction($GATEWAY["name"], $_POST, "SQL returned invalid data"); die(); }
-	$data=mysql_fetch_array($qry);
-print_r(	$data);
+        if (!$qry) {
+            logTransaction($GATEWAY["name"], array_merge($_POST, array('SQL query' => $qry)), "SQL query error");
+            //   die();
+            $paymentSuccess = false;
+        }
+        if (mysql_num_rows($qry) != 1) {
+            logTransaction($GATEWAY["name"], $_POST, "SQL returned invalid data");
+            // die();
+            $paymentSuccess = false;
+        }
+        $data = mysql_fetch_array($qry);
+        $order_amount = $data['total'];
 
-	$order_amount=$data['total'];
+        if (!empty($GATEWAY['convertto']))
+            if ($data['currency_id'] != $GATEWAY['convertto']) { // need to convert to another currency
+                $data['total'] = convertCurrency($data['total'], $data['currency_id'], $GATEWAY['convertto']);
+                $_POST['PAYMENT_AMOUNT'] = $_POST['PAYMENT_AMOUNT'];
+                $qry0 = mysql_query("SELECT code FROM tblcurrencies WHERE id=" . $GATEWAY['convertto']);
+                $data0 = mysql_fetch_array($qry0);
+                $data['currency_code'] = $data0['code'];
+            }
 
-	if(!empty($GATEWAY['convertto'])) if($data['currency_id']!=$GATEWAY['convertto']){	// need to convert to another currency
-		$data['total'] = convertCurrency($data['total'],$data['currency_id'],$GATEWAY['convertto']);
-		$_POST['PAYMENT_AMOUNT']=$_POST['PAYMENT_AMOUNT'];
-		$qry0=mysql_query("SELECT code FROM tblcurrencies WHERE id=".$GATEWAY['convertto']);
-		$data0=mysql_fetch_array($qry0);
-		$data['currency_code']=$data0['code'];
-	}
-	
 
-	if($_POST['PAYMENT_AMOUNT']==$data['total'] && $_POST['ZUPAYEE_ACC']==$GATEWAY['zupago_id'] && $_POST['ZUPAYEE_ACC_BTC']==$GATEWAY['zupago_id_btc'] && $_POST['CURRENCY_TYPE']==$data['currency_code']){
+        if ($_POST['PAYMENT_AMOUNT'] == $data['total'] && ($_POST['ZUPAYEE_ACC'] == $GATEWAY['zupago_id'] || $_POST['ZUPAYEE_ACC_BTC'] == $GATEWAY['zupago_id_btc']) && $_POST['CURRENCY_TYPE'] == $data['currency_code']) {
 
-		addInvoicePayment($invoiceid,$_POST['tokan'],$order_amount,$fee,$gatewaymodule); # Apply Payment to Invoice: invoiceid, transactionid, amount paid, fees, modulename
-		processPOST($apikey, $data['total'], $GATEWAY['zupago_id'], $GATEWAY['zupago_id_btc'], $data['currency_code']);
-		logTransaction($GATEWAY["name"],$_POST,"Successful"); # Save to Gateway Log: name, data array, status
-
-   }else{ // you can also save invalid payments for debug purposes
-
-		 processPOST($apikey, $data['total'], $GATEWAY['zupago_id'], $GATEWAY['zupago_id_btc'], $data['currency_code']);
-     logTransaction($GATEWAY["name"],$_POST,"Fake Data");
-
-   }
-
-}else{
-		
-	processPOST($apikey, 'not defined', $GATEWAY['zupago_id'], $GATEWAY['zupago_id_btc'], $data['currency_code']);
-	logTransaction($GATEWAY["name"],$_POST,"Unsuccessful"); # Save to Gateway Log: name, data array, status
-
+            addInvoicePayment($invoiceid, $_POST['tokan'], $order_amount, $fee, $gatewaymodule); # Apply Payment to Invoice: invoiceid, transactionid, amount paid, fees, modulename
+            processPOST($apikey, $data['total'], $GATEWAY['zupago_id'], $GATEWAY['zupago_id_btc'], $data['currency_code']);
+            logTransaction($GATEWAY["name"], $_POST, "Successful"); # Save to Gateway Log: name, data array, status
+            $paymentSuccess = true;
+        } else { // you can also save invalid payments for debug purposes
+            processPOST($apikey, $data['total'], $GATEWAY['zupago_id'], $GATEWAY['zupago_id_btc'], $data['currency_code']);
+            logTransaction($GATEWAY["name"], $_POST, "Fake Data");
+            $paymentSuccess = false;
+        }
+    } else {
+        $paymentSuccess = false;
+        processPOST($apikey, 'not defined', $GATEWAY['zupago_id'], $GATEWAY['zupago_id_btc'], $data['currency_code']);
+        logTransaction($GATEWAY["name"], $_POST, "Unsuccessful"); # Save to Gateway Log: name, data array, status
+    }
 }
+callback3DSecureRedirect($invoiceid, $paymentSuccess);
 ?>
--- upload/modules/gateways/zupago.php
+++ upload/modules/gateways/zupago.php
@@ -1,57 +1,60 @@
 <?php
 
-function zupago_config(){
+function zupago_config() {
 
     $configarray = array(
-     "FriendlyName" => array("Type" => "System", "Value"=>"ZuPago HyBrid (HD) Wallet"),
-		 "zupago_id" => array("FriendlyName" => "ZuPago Account", "Type" => "text", "Size" => "20", "Description" => "example: ZU-123456"),
-		 "zupago_id_btc" => array("FriendlyName" => "ZuPago Bitcoin Account", "Type" => "text", "Size" => "20", "Description" => "example: ZB-123456"),
-		 "zupago_pass" => array("FriendlyName" => "API Key", "Type" => "text", "Size" => "20", "Description" => "Renerate & Enable your APi Key in your ZP account"),
+        "FriendlyName" => array("Type" => "System", "Value" => "ZuPago HyBrid (HD) Wallet"),
+        "zupago_id" => array("FriendlyName" => "ZuPago Account", "Type" => "text", "Size" => "20", "Description" => "example: ZU-123456"),
+        "zupago_id_btc" => array("FriendlyName" => "ZuPago Bitcoin Account", "Type" => "text", "Size" => "20", "Description" => "example: ZB-123456"),
+        "zupago_pass" => array("FriendlyName" => "API Key", "Type" => "text", "Size" => "20", "Description" => "Renerate & Enable your APi Key in your ZP account"),
     );
-		return $configarray;
-
+    return $configarray;
 }
 
 function zupago_link($params) {
 
-	global $_LANG;
+    global $_LANG;
 
-	# Gateway Specific Variables
-	$zupago_id = $params['zupago_id'];
-	$zupago_id_btc = $params['zupago_id_btc'];
-	$zupago_pass = $params['zupago_pass'];
-	# Invoice Variables
-	$invoiceid = $params['invoiceid'];
-	$description = $params["description"];
-  $amount = $params['amount']; # Format: ##.##
-  $currency = $params['currency']; # Currency Code
+    # Gateway Specific Variables
+    $zupago_id = $params['zupago_id'];
+    $zupago_id_btc = $params['zupago_id_btc'];
+    $zupago_pass = $params['zupago_pass'];
+    # Invoice Variables
+    $invoiceid = $params['invoiceid'];
+    $description = $params["description"];
+    $amount = $params['amount']; # Format: ##.##
+    $currency = $params['currency']; # Currency Code
+    # System Variables
+    $companyname = $params['companyname'];
+    $systemurl = $params['systemurl'];
+    $currency = $params['currency'];
 
-	# System Variables
-	$companyname = $params['companyname'];
-	$systemurl = $params['systemurl'];
-	$currency = $params['currency'];
+    # Enter your code submit to the gateway...
+    if ($_SERVER['REQUEST_SCHEME'] == 'https') {
+        $http = explode('://', $systemurl);
+        if ($http[0] == 'http') {
+            $systemurl = 'https://' . $http[1];
+        }
+    }
 
-	# Enter your code submit to the gateway...
+    $code = '<form action="https://zupago.pe/api" method="post">
+<input type="hidden" name="SUGGESTED_MEMO" value="' . $description . '">
 
-	$code = '<form action="https://zupago.pe/api" method="post">
-<input type="hidden" name="SUGGESTED_MEMO" value="'.$description.'">
-
-<input type="hidden" name="PAYMENT_REF" value="'.$invoiceid.'" />
-<input type="hidden" name="PAYMENT_AMOUNT" value="'.$amount.'" />
-<input type="hidden" name="ZUPAYEE_ACC" value="'.$zupago_id.'" />
-<input type="hidden" name="ZUPAYEE_ACC_BTC" value="'.$zupago_id_btc.'" />
-<input type="hidden" name="ZUPAYEE_ACC_KEY" value="'.$zupago_pass.'" />
-<input type="hidden" name="CURRENCY_TYPE" value="'.$currency.'" />
-<input type="hidden" name="ZUPAYEE_NAME" value="'.$companyname.'" />
-<input type="hidden" name="SUCCESS_URL" value="'.$systemurl.'/viewinvoice.php?id='.$invoiceid.'&paymentsuccess=true" />
+<input type="hidden" name="PAYMENT_REF" value="' . $invoiceid . '" />
+<input type="hidden" name="PAYMENT_AMOUNT" value="' . $amount . '" />
+<input type="hidden" name="ZUPAYEE_ACC" value="' . $zupago_id . '" />
+<input type="hidden" name="ZUPAYEE_ACC_BTC" value="' . $zupago_id_btc . '" />
+<input type="hidden" name="ZUPAYEE_ACC_KEY" value="' . $zupago_pass . '" />
+<input type="hidden" name="CURRENCY_TYPE" value="' . $currency . '" />
+<input type="hidden" name="ZUPAYEE_NAME" value="' . $companyname . '" />
+<input type="hidden" name="SUCCESS_URL" value="' . $systemurl . '/modules/gateways/callback/zupago.php?page=success" />
 <input type="hidden" name="SUCCESS_URL_METHOD" value="LINK" />
-<input type="hidden" name="CANCEL_URL" value="'.$systemurl.'/viewinvoice.php?id='.$invoiceid.'&paymentfailed=true" />
+<input type="hidden" name="CANCEL_URL" value="' . $systemurl . '/modules/gateways/callback/zupago.php?page=cancel" />
 <input type="hidden" name="CANCEL_URL_METHOD" value="LINK" />
-<input type="hidden" name="STATUS_URL" value="'.$systemurl.'/modules/gateways/callback/zupago.php" />
-<input type="submit" value="'.$_LANG['invoicespaynow'].'" />
+<input type="submit" value="' . $_LANG['invoicespaynow'] . '" />
 </form>';
 
-	return $code;
-
+    return $code;
 }
+
 ?>
